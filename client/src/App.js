9254710851941a03e806b7e3c1e108ecab19e2ec
Mark Delgado <markdelgado94@gmail.com>
2021-04-17 14:17:31 -0700

    sign up and sign in working

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import SideBar from './Components/SideBar.js'
-import GCKingsCup from './Components/KingsCup'
-import BGHeader from './Components/Header'
-import Quarters from './Components/Quarters'
-import FlipCup from './Components/FlipCup'
-import BeerPong from './Components/BeerPong.js'
-import KingsCupAcc from './Components/GPKingsCup'
-import QuartersAcc from './Components/GPQuarters'
 import SignInCard from './Components/Login'
+import BGLogo from "./Components/Logo";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,39 +15,10 @@ export default function CenteredGrid() {
 
   return (
     <div className="App-header">
-      <KingsCupAcc/>
-      <QuartersAcc/>
+      <BGLogo />
       <div className={classes.root}>
         <SignInCard />
 
-        <Grid container spacing={2}
-          // direction="column"
-          // alignItems="flex center"
-          justify="center"
-          style={{ minHeight: '100vh', textAlign: "center", maxWidth: 466 }}
-        >
-          <Grid item xs={12}>
-      
-          </Grid>
-          <Grid item s={12}>
-            <BGHeader></BGHeader>
-            <SideBar />
-          </Grid>
-
-          <Grid item s={6}>
-            <GCKingsCup></GCKingsCup>
-          </Grid>
-          <Grid item s={6}>
-            <Quarters></Quarters>
-          </Grid>
-
-          <Grid item s={6}>
-            <FlipCup></FlipCup>
-          </Grid>
-          <Grid item s={6}>
-            <BeerPong></BeerPong>
-          </Grid>
-        </Grid>
       </div>
     </div>
   );
